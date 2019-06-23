3c18ea1d02d3bbd50f817f0d2fbeec1d0d004413
Alexey Popov <flyslam@yandex.ru>
2019-06-23 09:56:24 +0300

    New templates

--- index.php
+++ index.php
@@ -1,44 +1,71 @@
-<?php
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use directapi\DirectApiService;
-use directapi\services\campaigns\criterias\CampaignsSelectionCriteria;
-use directapi\services\campaigns\enum\CampaignStateEnum;
-use directapi\services\campaigns\enum\CampaignFieldEnum;
-
-$loader = require __DIR__ . '/vendor/autoload.php';
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
-
-$directApiService = new DirectApiService(getenv('ACCESS_TOKEN'), getenv('LOGIN'));
-$criteria = new CampaignsSelectionCriteria();
-$criteria->States = [CampaignStateEnum::ON];
-$campaigns = $directApiService->getCampaignsService()->get($criteria, CampaignFieldEnum::getValues());
-
-var_dump($campaigns);
-
-/*
-use Biplane\YandexDirect\Api\V5\Contract\AdFieldEnum;
-use Biplane\YandexDirect\Api\V5\Contract\AdsSelectionCriteria;
-use Biplane\YandexDirect\Api\V5\Contract\GetAdsRequest;
-use Biplane\YandexDirect\Api\V5\Contract\StateEnum;
-use Biplane\YandexDirect\Api\V4\Contract\NewForecastInfo;
-use Biplane\YandexDirect\User;
-
-$user = new User([
-    'access_token' => getenv('ACCESS_TOKEN'),
-    'login' => getenv('LOGIN'),
-    'locale' => User::LOCALE_RU,
-]);
-
-$params = NewForecastInfo::create()
-    ->setCurrency('RUB')
-    ->setPhrases(['test']);
-
-$response = $user->getApiService()->createNewForecast($params);
-
-var_dump($response); die();
-*/
-
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yandex API Forecast</title>
+    <link rel="stylesheet" href="/assets/css/main.min.css">
+</head>
+<body>
+<div class="main-back">
+    <div class="calc-section">
+        <div class="text">
+            <img src="/assets/images/num_1.png" alt="num_1">
+            <p class="title">Внимание эта методика подходит для 80% бизнесов</p>
+            <p>Проверьте, насколько она будет эффективно
+                <br>работать именно в вашем случае</p></div>
+        <div class="text">
+            <img src="/assets/images/num_2.png" alt="num_2">
+            <p class="title">Воспользуйтесь калькулятором и определите:</p>
+            <p>— Подходит ли вам данный метод
+                <br>— Сколько заявок и какую прибыль вы сможете получать</p></div>
+        <div class="calc">
+            <div class="title">Примерный расчет на основе данных «Прогноз бюджет Я.Директа»</div>
+            <div class="content">
+                <div class="poll">
+                    <div class="items">
+                        <div class="item active">
+                            <p>Фраза по которой могут искать
+                                <br>Ваш продукт в интернете</p>
+                            <input type="text" placeholder="Цемент оптом">
+                        </div>
+                        <div class="item">
+                            <p>Регион в котором вы работаете</p>
+                            <input type="text" id="region">
+                        </div>
+                        <div class="item">
+                            <p>Прибыль которую приносит
+                                <br>1 клиент</p>
+                            <input type="text" placeholder="1 000 000">
+                        </div>
+                        <div class="item">
+                            <p>Скольким из 10 потенциальных
+                                <br>клиентов обратившихся к вам
+                                <br>вы сможете продать свой продукт</p>
+                            <input type="text" placeholder="5">
+                        </div>
+                    </div>
+                    <div class="bar">
+                        <div></div>
+                    </div>
+                    <p>*Фраза должна состоять из 2 или 3 слов (максимум 4)</p>
+                </div>
+            </div>
+            <div class="buttons">
+                <div class="help modal-btn" data-modal="Будущая справка">Справка</div>
+                <div class="next">Далее <img src="/assets/images/arrow_right.png" alt="arrow"></div>
+            </div>
+            <div class="modal">
+                <div>
+                    <img src="/assets/images/close.png" alt="close">
+                    <div class="text"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="assets/js/app.min.js"></script>
+</body>
+</html>
