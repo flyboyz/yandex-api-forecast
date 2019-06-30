6016a9c37f97eabaa280f191aa0e30fce8c70e48
Alexey Popov <flyslam@yandex.ru>
2019-06-30 13:43:48 +0300

    Add error logic

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -112,26 +112,39 @@ $(function () {
         $.getJSON("/api/createNewForecast.php", {
             phrases: $('#phrases').val(),
             region: $('#region-id').val()
-        }, function (data) {
-            if (data.hasOwnProperty('data')) {
-                getForecast(data.data);
+        }, function (foreсastId) {
+            if (parseInt(foreсastId) > 0) {
+                getForecast(foreсastId);
+            } else {
+                console.log('Error: createNewForecast');
+                showError('Ошибка создания отчёта', 'Ошибка получения ForecastID');
             }
         });
     }
 
-    function getForecast(foreCastId) {
+    function getForecast(foreсastId) {
         $calcTitle.text('Загрузка прогноза');
 
-        $.getJSON("/api/getForecast.php", {id: foreCastId}, function (data) {
-            calcucating(data.Common);
+        $.getJSON("/api/getForecast.php", {id: foreсastId}, function (foreсastData) {
+            if (foreсastData === '0') {
+                console.log('Error: getForecast');
+                showError('Ошибка загрузки отчёта', `Ошибка получения Forecast с id ${foreсastId}`);
+            } else {
+                calcucating(foreсastData);
+                $.getJSON("/api/deleteForecastReport.php", {id: foreсastId}, function (isDeleted) {
+                    if (parseInt(isDeleted) === 0) {
+                        console.log('Error: deleteForecastReport');
+                    }
+                });
+            }
         });
     }
 
     function calcucating(data) {
         const CR1 = .05;
 
-        let clicks = data.Clicks * 2;
-        let SRS = data.Max * .18 / 4;
+        let clicks = data.clicks * 2;
+        let SRS = data.price * .18 / 4;
         SRS = SRS < 10 ? 10 : SRS;
         let CR2 = 10 * $('#leads').val();
         let money = $('#money').val();
@@ -142,12 +155,15 @@ $(function () {
         let moneyPerMonth = moneyPerLead * CountOfSalesPerMonth;
 
         if (clicks <= 60 ) {
+            $calcTitle.text('В вашей теме слишком мало запросов по данной ключевой фразе').css('text-align', 'center');
             $calc.find('.result .few-clicks').addClass('active');
         } else if (SAS >= money) {
             $calc.find('.result .unfavorable').addClass('active');
         } else if (money >= 1 && money <= 100) {
             $calc.find('.result .normal').addClass('active');
         } else {
+            $calcTitle.text('Примерный расчет на основе данных «Прогноз бюджет Я.Директа»');
+
             $('span.money-per-month').text(moneyPerMonth.toString().replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 '));
             $('span.count-of-request-per-month').text(CountOfRequestPerMonth);
             $('span.count-of-sales-per-month').text(CountOfSalesPerMonth);
@@ -155,5 +171,10 @@ $(function () {
 
             $calc.find('.result .good').addClass('active');
         }
+    }
+
+    function showError(errorTitle, errorText) {
+        $calcTitle.text(errorTitle);
+        $calc.find('.error p').text(errorText);
     }
 });
